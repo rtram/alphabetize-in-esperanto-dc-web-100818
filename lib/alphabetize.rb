ec7730a70e6797d90812046ee2f76c8dd9014183
Robin Tram <robin.tram@gmail.com>
2018-10-05 19:57:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  # split esperanto alphabet
+  e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
+  ascii = "ABabcdefghijklmnopqrstuvwxyz".split(//)
+  converter = {}
+  counter = 0
+  
+  #iterate through e_alphabet and make a hash with e_alphabet as keys and ASCII as values
+  while counter < e_alphabet.length
+    converter[e_alphabet[counter]] = ascii[counter]
+    counter += 1 
+  end 
+  
+  #iterate through given array to replace e_alphabet with ASCII
+  arr.collect! do |string|
+    string.tr(" ", "_")
+      binding.pry
+  
+  end 
+
+  
+  #sort strings by ASCII
+  
+  #iterate through ASCII to convert back to e_alphabet
+
 end
